dbac31f21ae799b2339d47c077b6a827e7fd27db
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2026-05-09 20:16:59 +0530

    positioning fix

--- components/project/Card.tsx
+++ components/project/Card.tsx
@@ -21,7 +21,6 @@ function Card({ Title, Desc, SrcLink, WebLink, Skills, Imglink }: Cardprops) {
         <div className="flex items-center justify-start">
           <h3 className="mt-[4px] ml-[8px] transform text-[18px] leading-6 font-semibold text-black transition-all duration-500 ease-in-out hover:scale-105 hover:text-rose-600 lg:w-fit dark:text-white">
             <a href={WebLink} target="_blank">
-              <span className="absolute inset-0 max-w-xs" />
               {Title}
             </a>
           </h3>
